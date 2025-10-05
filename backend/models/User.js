03421f028d3da61fa8c0dd8b9837e2e6dd34ef6e
Manu Caporaso <manulpda@gmail.com>
2025-10-05 00:15:01 -0300

    se agregan roles de usuario

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,35 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database");
 
-const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "editor", "user"),
+      defaultValue: "user", // por defecto un usuario común
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("admin", "editor", "user"),
-    defaultValue: "user", // por defecto un usuario común
-  },
-});
+  {
+    timestamps: true,
+    tableName: "users",      // fuerza que la tabla se llame "users"
+    freezeTableName: true,   // evita que Sequelize pluralice el nombre
+  }
+);
 
 module.exports = User;

9962a80fcd0a6b93478942360e3390b153450e36
Manu Caporaso <manulpda@gmail.com>
2025-10-06 16:08:58 -0300

    Se cambia la duracion del JWT

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -44,7 +44,7 @@ router.post("/login",  async (req, res) => {
     const token = jwt.sign(
       { userId: user.id, email: user.email, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: "24h" }
     );
 
     res.json({
